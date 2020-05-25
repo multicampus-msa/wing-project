9ff060a08ff5260c1a613b9f48b66d087737f5ef
StolenMoments <unknown9732@naver.com>
2020-05-25 10:24:28 +0900

    트랜잭션 데이터 API 유저 정보 추가

--- server/src/main/java/wing/api/domain/transactionData/TransactionData.java
+++ server/src/main/java/wing/api/domain/transactionData/TransactionData.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import wing.api.domain.artist.Artist;
+import wing.api.domain.user.User;
 
 import javax.persistence.*;
 
@@ -18,9 +19,9 @@ public class TransactionData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
 
-//    @ManyToOne(targetEntity = User.class)
-//    @JoinColumn(name="user_id")
-//    private User user;
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name="user_id")
+    private User user;
 
     @ManyToOne(targetEntity = Artist.class)
     @JoinColumn(name="artist_id")
@@ -30,21 +31,17 @@ public class TransactionData {
     private String datetime;
     private String uid;
 
-    @ColumnDefault("default")
-    private Long userId;
-
 
     // TODO
     // User user 추가하기
     @Builder
-    public TransactionData(Artist artist, String uid, Long amount, String datetime) {
-
+    public TransactionData(User user, Artist artist, String uid, Long amount, String datetime) {
+        this.user = user;
         this.artist = artist;
         this.uid = uid;
         this.amount = amount;
         this.datetime = datetime;
 
     }
-
 
 }
