be48c721a2494a94967b32d29cc59d820b00b86c
profornnan <profornnan@naver.com>
2020-05-27 16:11:44 +0900

    CharList 컴포넌트 hook으로 변경

--- client/src/components/Live/ChatTemplate/ChatList.js
+++ client/src/components/Live/ChatTemplate/ChatList.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import socketio from 'socket.io-client'; 
 
 const socket = socketio.connect('http://localhost:3001');
 
+function ChatList(props) {
 
-class ChatList extends Component {
-    
-    constructor(props){
-        super(props); 
-        this.state = {
-            logs : []
-        }
-    }
+    const [logs, setLogs] = useState([]);
 
-    componentDidMount(){
-        socket.emit('joinRoom', {roomName: this.props.playUrl});
+    useEffect(() => {
+        socket.emit('joinRoom', {roomName: props.playUrl});
         socket.on('message', (data) =>{
-            const logs2 = this.state.logs; 
-            data.key = 'key_' + (this.state.logs.length+1); 
-            console.log(data); 
-            logs2.unshift(data);   
-            this.setState({logs : logs2}); 
-        }); 
-    }
-    
-    render() {
+            setLogs(logs.concat({
+                ...data,
+                key: 'key_' + (logs.length + 1)
+            }));
+        });
+    })
 
-        const messages = this.state.logs.map(e=>(
-            <div key={e.key} >
-                <img alt="user icon" src = "man.png"  width="25px" height="25px" style={{marginLeft:'5px'}}/>
-                <span style={{color : '#819FF7' , fontWeight : 'bold' }} > {e.name} </span>
-                <span>     { e.message } </span>
-                <p style={{clear:'both'}} />
-            </div>
-        ))
-
-        return (
-            <div style = {{backgroundColor : '#f2f2f2', marginTop:'80px', marginBottom:'15px', height : '800px'}}>
-                {messages}
-            </div>
-        );
-    }
+    return (
+        <div style = {{backgroundColor : '#f2f2f2', marginTop:'80px', marginBottom:'15px', height : '800px'}}>
+            {
+                logs.map(e=>(
+                    <div key={e.key} >
+                        <img alt="user icon" src = "man.png"  width="25px" height="25px" style={{marginLeft:'5px'}}/>
+                        <span style={{color : '#819FF7' , fontWeight : 'bold' }} > {e.name} </span>
+                        <span>     { e.message } </span>
+                        <p style={{clear:'both'}} />
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
 export default ChatList;
