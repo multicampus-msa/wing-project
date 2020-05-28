b0b14444102c8ba2085488cf40dfb9aae325f590
profornnan <profornnan@naver.com>
2020-05-28 15:05:17 +0900

    채팅 입력 창 수정 (사용자 프로필, 이름 추가, UI 개선)

--- client/src/components/Live/ChatTemplate/ChatInput.js
+++ client/src/components/Live/ChatTemplate/ChatInput.js
@@ -1,29 +1,65 @@
 import React, {useState, useContext} from 'react';
-import {MdSend} from 'react-icons/md';
-import {Button} from 'reactstrap'; 
 import socketio from 'socket.io-client';
 import UserContext from "../../Context/UserContext";
 
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      padding: '2px 4px',
+      display: 'block',
+      alignItems: 'center',
+      width: 400,
+      fontFamily: "NanumSquare",
+    },
+    name: {
+        fontSize: '14px',
+        marginLeft: '8px',
+    },
+    inputWrapper: {
+        display: 'flex',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+}));
+
 const ChatInput = ({playUrl}) => {
-    const [name, setName] = useState(''); 
-    const [value, setValue] = useState(''); 
+    const classes = useStyles();
+    const [message, setMessage] = useState(''); 
     const socket = socketio.connect('http://localhost:3001');
     const userState = useContext(UserContext);
 
-    const handleName = (e) => {
-        setName(e.target.value); 
-    }
-    
     const handleChange = (e) =>{ // input 창 입력 변화 이벤트 처리 
-        setValue(e.target.value); 
+        setMessage(e.target.value); 
     }
     
     const handleClick = () => { // 보내기 버튼 클릭 시 이벤트 처리 
-        console.log(value);
-        socket.emit('joinRoom', {roomName: playUrl});
-        const imageUrl = userState.imageUrl;
-        socket.emit('message', {name : name, message : value, imageUrl: imageUrl}); 
-        setValue('');
+        if (userState.name === '') {
+            alert('로그인이 필요한 기능입니다.')
+        }
+        else if (message === '') {
+            alert('메시지를 입력하세요.')
+        }
+        else {
+            socket.emit('joinRoom', {roomName: playUrl});
+            const {name, imageUrl} = userState;
+            socket.emit('message', {name : name, message : message, imageUrl: imageUrl}); 
+        }
+        setMessage('');
     }
 
     const handleKeypress = (e) => { // 엔터를 이용하여 전송할 경우 이벤트 처리 
@@ -33,10 +69,28 @@ const ChatInput = ({playUrl}) => {
     }
 
     return (
-        <div style = {{display : 'flex'}}>
-            <input name = {name} onChange = {handleName} onKeyPress = {handleKeypress} placeholder="닉네임을 입력하세요" style={{marginRight : '5px'}}/>
-            <input placeholder="메시지를 입력하세요" value = {value} onChange = {handleChange} onKeyPress={handleKeypress} style={{marginRight:'5px', width:'auto'}}/>
-            <Button outline color = "info" size="sm" onClick= {handleClick}> <MdSend /> </Button>
+        <div style={{display: 'flex'}}>
+            <Avatar src={userState.imageUrl} style={{width: '45px', height: '45px', marginLeft: '5px', marginTop: '5px'}}/>
+            <div className={classes.root}>
+                <div className={classes.name}>
+                    {
+                        userState.name === '' ? 'Unknown User' : userState.name  
+                    }
+                </div>
+                <div className={classes.inputWrapper}>
+                    <InputBase
+                        className={classes.input}
+                        placeholder="메시지를 입력하세요"
+                        value={message}
+                        onChange={handleChange}
+                        onKeyPress={handleKeypress}
+                    />
+                    <IconButton className={classes.iconButton} aria-label="send" onClick={handleClick}>
+                        <SendIcon />
+                    </IconButton>
+                    <Divider className={classes.divider} orientation="vertical" />
+                </div>
+            </div>
         </div>
     );
 };
