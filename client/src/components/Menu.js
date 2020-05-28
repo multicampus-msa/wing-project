ef9a107067a835d701c10b530495adbfa57003e4
StolenMoments <unknown9732@naver.com>
2020-05-28 10:25:09 +0900

    상단 로고, 로그인 버튼 위치 변경

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,33 +1,52 @@
-import React, { Fragment, useEffect, createContext, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components'
 import MenuButtons from "./Main/MenuButtons";
-// import MainLoginButton from "./Main/MainLoginButton";
-//import Login from './Login/Login';
 import axios from 'axios'
 import LOGO_URL from "./Constants/LOGO_URL";
 import LoginButton from './Main/LoginButton';
 import Root from './Root'
 import UserContext from "./Context/UserContext";
 
+const LogoDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 200px);
+  grid-template-rows: 80px 1px 1px;
+  justify-content: center;
+  
+  
+  .logo {
+    grid-column: 3;
+    grid-row: 1;
+    margin-left: -5rem;
+  }
+  
+  .login {
+    margin-left: 5.5rem;
+    grid-column: 5;
+    grid-row: 2;
+  }
+  
+`
 
-const StyledDiv = styled.div`
+const ButtonDiv = styled.div`
+
     display: flex;
     justify-content: center;
     margin-left: auto;
     margin-right: auto;
-`;
+`
 
 // todo : 로그인 버튼 위치 수정 필요
-const BtnStlye = styled.div`
+const BtnStyle = styled.div`
     margin-left: 88rem;
     margin-bottom: -1rem;
 `;
 
-const Menu = ({history}) => {
+const Menu = ({ history }) => {
 
-    const [userId, setUserId] = useState(null); 
-    const [token, setToken] = useState(null); 
+    const [userId, setUserId] = useState(null);
+    const [token, setToken] = useState(null);
     const [name, setName] = useState("");// 로그인 된 사용자 정보 
     const [auth2, setAuth2] = useState(null);
     const [userState, setUserState] = useState({
@@ -36,11 +55,11 @@ const Menu = ({history}) => {
         email: ""
     })
 
-    const login=()=>{
-        auth2.signIn().then(googleUser=>{
-            let profile = googleUser.getBasicProfile();           
-            const data = {      
-                userId: profile.getId(),        
+    const login = () => {
+        auth2.signIn().then(googleUser => {
+            let profile = googleUser.getBasicProfile();
+            const data = {
+                userId: profile.getId(),
                 name: profile.getName(),
                 email: profile.getEmail(),
                 imageUri: profile.getImageUrl(),
@@ -57,12 +76,12 @@ const Menu = ({history}) => {
             const targetUrl = 'http://localhost:8080/api/user/save'
             console.log(data);
             axios.post(targetUrl, data)
-            .then(response =>{                  
-                console.log(response)
-            })
-            .catch(function(error){
-                console.log(error);
-            })
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
             // console.log('ID : '+profile.getId());
             // console.log('Name : '+profile.getName());
             // console.log('Image URL : '+profile.getImageUrl());
@@ -70,11 +89,11 @@ const Menu = ({history}) => {
             setUserId(profile.getId());
             setName(profile.getName())
             setToken(googleUser.getAuthResponse().id_token)
-            
+
         })
     }
-    const logout=()=>{
-        setToken('')        
+    const logout = () => {
+        setToken('')
         console.log("로그아웃 합니다");
         auth2.disconnect();
         setUserId(null);
@@ -83,10 +102,10 @@ const Menu = ({history}) => {
         history.push(`/`);
 
     }
-    const googleSDK=()=>{
+    const googleSDK = () => {
         // platform.js 스크립트 로드 후 .. 
-        window['googleSDKLoaded']=()=>{
-            window['gapi'].load('auth2', ()=>{
+        window['googleSDKLoaded'] = () => {
+            window['gapi'].load('auth2', () => {
                 setAuth2(window['gapi'].auth2.init({
                     client_id: '4141518158-olfgnnv29g8163c0v5saq6dorpg806l2.apps.googleusercontent.com',
                     cookiepolicy: 'single_host_origin',
@@ -94,34 +113,41 @@ const Menu = ({history}) => {
                 }));
             })
         }
-        (function(d,s,id){
+        (function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
-            if(d.getElementById(id)){return;}
+            if (d.getElementById(id)) {
+                return;
+            }
             js = d.createElement(s);
             js.id = id;
             js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
-            fjs.parentNode.insertBefore(js, fjs);  
-        }(document, 'script', 'google-jssdk'));        
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'google-jssdk'));
     }
-    useEffect(()=>{ // token 값이 업데이트 될 때만 실행
+    useEffect(() => { // token 값이 업데이트 될 때만 실행
         googleSDK()
         //console.log(auth2)
-    },[token]);
+    }, [token]);
 
     return (
         <UserContext.Provider value={userState}>
             <Fragment>
-                <StyledDiv>
-                    <Link to="/"><img src={LOGO_URL} alt="Win:G 로고"/></Link>
-                </StyledDiv>
+                <LogoDiv>
+                    <div className="logo">
+                        <Link to="/"><img src={LOGO_URL}
+                                          alt="Win:G 로고"
+                                          style={{ width: "330px", height: "120px" }}
+                        /></Link>
+                    </div>
+                    <div className="login">
+                        <LoginButton token={token} name={name} login={login} logout={logout}/>
+                    </div>
+                </LogoDiv>
                 <br/>
-                <BtnStlye>
-                    <LoginButton token={token} name={name} login={login} logout={logout}/>
-                </BtnStlye>
                 <br/>
-                <StyledDiv>
-                    <MenuButtons />
-                </StyledDiv>
+                <ButtonDiv>
+                    <MenuButtons/>
+                </ButtonDiv>
                 <hr/>
                 <Root/>
             </Fragment>
