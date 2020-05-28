2d9444c2c331159f735893dbc6cae11bed2d0479
profornnan <profornnan@naver.com>
2020-05-28 13:43:04 +0900

    채팅 입력 시 스크롤 최하단 이동 구현

--- client/src/components/Live/ChatTemplate/ChatList.js
+++ client/src/components/Live/ChatTemplate/ChatList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -17,9 +17,14 @@ const useStyles = makeStyles((theme) => ({
 
 const ChatList = ({logs}) => {
     const classes = useStyles();
+    const listEl = useRef(null);
+
+    useEffect(() => {
+        listEl.current.scrollTo(0, listEl.current.scrollHeight)
+    })
 
     return (
-        <List className={classes.root}>
+        <List className={classes.root} ref={listEl}>
             {
                 logs.map((value) => (
                     <ListItem key={value.key}>
